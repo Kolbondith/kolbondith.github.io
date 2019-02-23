1543d6bf9419c30a4978eaefbcb22e91832f1c17
Kolbondith <47352788+Kolbondith@users.noreply.github.com>
2019-02-23 21:13:28 +0700

    Update customer.php

--- customer.php
+++ customer.php
@@ -1,9 +1,91 @@
 {
-	names=[
-	{"name":"Bondith","province":"PhnomPenh"},
-	{"name":"Vira","province":"PhnomPenh"},
-	{"name":"Ponler","province":"Kompot"},
-	{"name":"Vanna","province":"BatDomBong"},
-	{"name":"Vichea","province":"BonteayMeanChey"}
-	]
+  "records": [
+    {
+      "Name": "Alfreds Futterkiste",
+      "City": "Berlin",
+      "Country": "Germany"
+    },
+    {
+      "Name": "Ana Trujillo Emparedados y helados",
+      "City": "México D.F.",
+      "Country": "Mexico"
+    },
+    {
+      "Name": "Antonio Moreno Taquería",
+      "City": "México D.F.",
+      "Country": "Mexico"
+    },
+    {
+      "Name": "Around the Horn",
+      "City": "London",
+      "Country": "UK"
+    },
+    {
+      "Name": "B's Beverages",
+      "City": "London",
+      "Country": "UK"
+    },
+    {
+      "Name": "Berglunds snabbköp",
+      "City": "Luleå",
+      "Country": "Sweden"
+    },
+    {
+      "Name": "Blauer See Delikatessen",
+      "City": "Mannheim",
+      "Country": "Germany"
+    },
+    {
+      "Name": "Blondel père et fils",
+      "City": "Strasbourg",
+      "Country": "France"
+    },
+    {
+      "Name": "Bólido Comidas preparadas",
+      "City": "Madrid",
+      "Country": "Spain"
+    },
+    {
+      "Name": "Bon app'",
+      "City": "Marseille",
+      "Country": "France"
+    },
+    {
+      "Name": "Bottom-Dollar Marketse",
+      "City": "Tsawassen",
+      "Country": "Canada"
+    },
+    {
+      "Name": "Cactus Comidas para llevar",
+      "City": "Buenos Aires",
+      "Country": "Argentina"
+    },
+    {
+      "Name": "Centro comercial Moctezuma",
+      "City": "México D.F.",
+      "Country": "Mexico"
+    },
+    {
+      "Name": "Chop-suey Chinese",
+      "City": "Bern",
+      "Country": "Switzerland"
+    },
+    {
+      "Name": "Comércio Mineiro",
+      "City": "São Paulo",
+      "Country": "Brazil"
+    }
+  ]
 }
+Example
+The ng-repeat directive is perfect for looping through an array:
+
+<div ng-app="myApp" ng-controller="customersCtrl"> 
+
+<ul>
+  <li ng-repeat="x in myData">
+    {{ x.Name + ', ' + x.Country }}
+  </li>
+</ul>
+
+</div>
