dd5b008bd0d14795b5b366507972368bb29feb0b
Kolbondith <47352788+Kolbondith@users.noreply.github.com>
2019-02-23 21:04:26 +0700

    Create customer.php

---
+++ customer.php
@@ -0,0 +1,9 @@
+{
+	names=[
+	{"name":"Bondith","province":"PhnomPenh"},
+	{"name":"Vira","province":"PhnomPenh"},
+	{"name":"Ponler","province":"Kompot"},
+	{"name":"Vanna","province":"BatDomBong"},
+	{"name":"Vichea","province":"BonteayMeanChey"}
+	]
+}
